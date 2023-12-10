54076845b153788295ebeb7181e116ce43bd17a3
Boolean <valarcon@autonoma.edu.pe>
2023-12-10 12:56:19 -0500

    Test 2

--- MyClass.java
+++ MyClass.java
@@ -1,5 +1,37 @@
 public class MyClass {
   public static void main(String[] args) {
     System.out.println("Hello World");
+    System.out.println(2*5);
+
+    boolean TrueThis = false;
+    boolean FalseThis = true;
+
+    System.out.println(TrueThis);
+    System.out.println(FalseThis);
+
+    int x = 5;
+    int y = 6;
+    int z = x + y;
+
+    System.out.println(z);
+    System.out.println(x);
+    System.out.println(y);
+
+    int a = 5, b = 6, c = 7;
+    System.out.println(a + b + c);
+
+    String H = "Hello";
+    String W = "World";
+    String SB = " ";
+    System.out.println(H + SB + W);
+
+    char myGrade = 'B';
+    System.out.println(myGrade);
   }
 }
+
+// This is a comment, in Java, comments start with ""//"" 
+
+/*
+    This is a multi-line comment try to best to
+ */
